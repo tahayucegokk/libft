36e404938232652df411bac03f103010f9534b2a
tahayucegokk <you@example.com>
2023-08-23 01:48:45 +0300

    new files added

--- ft_memset.c
+++ ft_memset.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muyucego <muyucego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/18 02:45:18 by muyucego          #+#    #+#             */
-/*   Updated: 2023/08/23 01:48:12 by muyucego         ###   ########.fr       */
+/*   Created: 2023/08/23 01:39:19 by muyucego          #+#    #+#             */
+/*   Updated: 2023/08/23 01:48:14 by muyucego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-
-int		ft_atoi(const char *str);
-size_t	ft_strlen(const char *str);
 void	*ft_memset(void *b, int c, size_t len)
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
+{
+	size_t	i;
 
-#endif
+	i = 0;
+	while (i < len)
+	{
+		((unsigned char *)b)[i] = c;
+		i++;
+	}
+	return (b);
+}
