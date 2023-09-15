a71c7e1007d0238b70346b02b48e910520afca49
tahayucegokk <you@example.com>
2023-09-16 01:31:25 +0300

    bzero added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: muyucego <muyucego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 02:45:18 by muyucego          #+#    #+#             */
-/*   Updated: 2023/08/23 01:48:12 by muyucego         ###   ########.fr       */
+/*   Updated: 2023/09/16 01:27:59 by muyucego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
